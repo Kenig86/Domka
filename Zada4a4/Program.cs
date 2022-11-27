c0ea0e700f359cd8795d4d2fdf9ed143e6b3167d
Vitaliy <ostarkov.ken@gmail.com>
2022-11-27 16:41:45 +0500

    Максимальное число из трех

---
+++ Zada4a4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if ((a > b) && (a > c))
+max = a;
+else if ((b > a) && (b > c))
+max = b;
+else if ((c > a) && (c > b))
+max = c;
+
+Console.WriteLine($"Максимальное число: {max}");
