7ef7451f9a8903153086a692d3edfe70619f34ef
Vitaliy <ostarkov.ken@gmail.com>
2022-11-27 16:35:30 +0500

    Нахождение большего и меньшего числа из двух

---
+++ Zada4a2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Большее число: {a}");
+    Console.WriteLine($"Меньшее число: {b}");
+}
+else
+{
+    Console.WriteLine($"Большее число: {b}");
+    Console.WriteLine($"Меньшее число: {a}");
+}
