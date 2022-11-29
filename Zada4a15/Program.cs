2f382b826116edd98caa4af8781003723ba96606
Vitaliy <ostarkov.ken@gmail.com>
2022-11-29 15:57:21 +0500

    Является ли этот день недели выходным?

---
+++ Zada4a15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a < 1 || a > 7){
+Console.WriteLine("Это не день недели.\nВведите день недели: ");
+a = Convert.ToInt32(Console.ReadLine());
+}
+if (a == 6 || a == 7)
+Console.WriteLine("Этот день выходной.");
+else Console.WriteLine("Этот день не выходной.");
