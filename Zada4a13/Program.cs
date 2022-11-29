5101345869b672aa92c7b80d15c2cd4324310cdf
Vitaliy <ostarkov.ken@gmail.com>
2022-11-29 15:52:10 +0500

    Выводит третью цифру или сообщает что еёнет

---
+++ Zada4a13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0) n = n * -1;
+else 
+if (n < 99){
+Console.WriteLine("Нет третьей цифры\nВведите трехзначное число: ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+while (n > 1000)
+    n/=10;
+Console.WriteLine(n % 10);
