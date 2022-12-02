16b8d1e23fa0345127c6a302810b2106795d757c
Vitaliy <ostarkov.ken@gmail.com>
2022-12-02 22:31:14 +0500

    Расстояние между двух точек в 3D пространстве

---
+++ Zada4a21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату точки А: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+double xb = Convert.ToDouble(Console.ReadLine());
+double xc = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату точки В: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+double yb = Convert.ToDouble(Console.ReadLine());
+double yc = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(xa - ya, 2) + Math.Pow(xb - yb, 2) + Math.Pow(xc - yc, 2));
+Console.WriteLine($"Расстояние между точками: {Math.Round(result, 2)}");
