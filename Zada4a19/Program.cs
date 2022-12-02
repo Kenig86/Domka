8a92e3009622894cc8fbb3534f6f11019ffe42c8
Vitaliy <ostarkov.ken@gmail.com>
2022-12-02 22:23:50 +0500

    Проверка является ли число палиндромом

---
+++ Zada4a19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пять чисел: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a / 10000 == a % 10 && (a / 1000) % 10 == (a / 10) % 10) 
+ Console.WriteLine("Палиндром"); 
+ else 
+ Console.WriteLine("Не палиндром");
