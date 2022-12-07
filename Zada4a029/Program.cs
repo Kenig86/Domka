0dd469b07c3b99d44a39002d1ca10e6f74bcac40
Vitaliy <ostarkov.ken@gmail.com>
2022-12-07 18:59:56 +0500

    Запрашивает элементы массива и выводит их на экран

---
+++ Zada4a029/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int [] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+   Console.Write($" Введите элементы массива номер {i} :\t ");
+   numbers[i] = Convert.ToInt32(Console.ReadLine());
+ }
+
+Console.WriteLine("Вывод массива");
+for (int i = 0; i < numbers.Length; i++)
+
+Console.Write($"{numbers[i]} ");
