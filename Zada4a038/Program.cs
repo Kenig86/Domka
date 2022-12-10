b76638a5a6da23dcc985b6179ed13eddadee5f63
Vitaliy <ostarkov.ken@gmail.com>
2022-12-10 20:24:45 +0500

    Разница между максимальным и минимальным элементом массива

---
+++ Zada4a038/Program.cs
@@ -0,0 +1,33 @@
+﻿// 38
+
+double MaxMin(double[] array)
+{
+    double a = 0, Max = array[0], Min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > Max)
+            Max =  array[i];
+            else
+            Min = array[i];
+
+    }
+    a = Max - Min;
+ 
+    return a;
+    
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Ведите {i + 1} элемент массива: ");
+    array[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+
+Console.WriteLine($"Разниуа между max и min элементами массива: {MaxMin(array)}");
