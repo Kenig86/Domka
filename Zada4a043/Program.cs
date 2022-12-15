f1705adac08ffb60bfbd2f7931308f30eab22fff
Vitaliy <ostarkov.ken@gmail.com>
2022-12-15 11:57:21 +0500

    Точка пересечения двух прямых

---
+++ Zada4a043/Program.cs
@@ -0,0 +1,23 @@
+﻿// 43
+
+Console.Clear();
+Console.WriteLine("Введите коэффициент k1 для первого уравнения:");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b1 для первого уравнения:");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите коэффициент k2 для второго уравнения:");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2 для второго уравнения:");
+double b2 = double.Parse(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2)
+    Console.WriteLine("Прямые совпадают");
+else if (k1 == k2)
+    Console.WriteLine("Прямые параллельны");
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения: ({x}, {y})");
+}
