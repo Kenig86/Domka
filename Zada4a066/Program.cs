5d85521dd94ea60f9482cb4c1f914bc3e372bf1d
Vitaliy <ostarkov.ken@gmail.com>
2022-12-26 14:24:25 +0500

    Сумма из промежутка чисел

---
+++ Zada4a066/Program.cs
@@ -0,0 +1,13 @@
+﻿int rec(int a, int b)
+{
+    if (a > b)
+        return 0;
+    return rec(a + 1, b) + a;
+}
+Console.Clear();
+Console.WriteLine("Введите два числа");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов в промежутке ({m},{n}) равна {rec(m, n)}");
